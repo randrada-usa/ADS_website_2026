290649dc3c7a29b5e86770c5b3ef40f81881cd48
JL <cocoasaurjl@gmail.com>
2026-09-07 13:07:25 +0800

    Refine homepage About section orbs

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -166,7 +166,8 @@ export default async function Home() {
       <ValuesMarquee />
       <section id="about-home" className="section container about-preview">
         <div className="about-background-design" aria-hidden="true">
-          <span className="about-bg-circle about-bg-circle-lower-left" />
+          <span className="about-bg-circle about-bg-circle-small-one" />
+          <span className="about-bg-circle about-bg-circle-small-two" />
           <svg
             className="about-bg-contours about-bg-contours-lower-right"
             viewBox="0 0 420 360"
