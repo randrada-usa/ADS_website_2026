cf78f685b3847898be1e3135d3089a6b3d498be4
JL <cocoasaurjl@gmail.com>
2026-09-07 22:38:16 +0800

    Update background circles for improved layout and visibility

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -166,8 +166,8 @@ export default async function Home() {
       <ValuesMarquee />
       <section id="about-home" className="section container about-preview">
         <div className="about-background-design" aria-hidden="true">
-          <span className="about-bg-circle about-bg-circle-small-one" />
-          <span className="about-bg-circle about-bg-circle-small-two" />
+          <span className="about-bg-circle about-bg-circle-upper-left" />
+          <span className="about-bg-circle about-bg-circle-upper-right" />
           <svg
             className="about-bg-contours about-bg-contours-lower-right"
             viewBox="0 0 420 360"
